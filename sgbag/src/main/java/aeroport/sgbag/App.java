7975184ca1b03dc46c1b16fa0aa7e02249523049
Arnaud Lahache <arnaud.lahache@gmail.com>
2011-11-23 21:13:02 +0100

    First commit : pom.xml and HelloWorld

---
+++ sgbag/src/main/java/aeroport/sgbag/App.java
@@ -0,0 +1,22 @@
+package aeroport.sgbag;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	
+	@Getter
+	@Setter
+	private String exemple;
+	
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+    }
+}
