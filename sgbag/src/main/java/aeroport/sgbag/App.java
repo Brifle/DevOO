3d3468718026010a142ae8e241a34eb00f915ad9
Arnaud Lahache <arnaud.lahache@gmail.com>
2011-11-23 21:34:38 +0100

    test

--- sgbag/src/main/java/aeroport/sgbag/App.java
+++ sgbag/src/main/java/aeroport/sgbag/App.java
@@ -14,7 +14,7 @@ public class App {
 	private String exemple;
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("Hello World! This is a test.");
 
 	}
 }
