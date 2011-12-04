bc69c565c52407ac54dcc0017eb9474c51211634
tpatel <thibaut.patel@gmail.com>
2011-12-04 23:04:07 +0100

    Classe Circuit avec méthode update + classe de test

--- sgbag/src/main/java/aeroport/sgbag/kernel/Circuit.java
+++ sgbag/src/main/java/aeroport/sgbag/kernel/Circuit.java
@@ -1,21 +1,39 @@
 package aeroport.sgbag.kernel;
 
-import java.util.Vector;
+import java.util.*;
+import lombok.*;
 
+
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
 public class Circuit {
 
-    /**
-   * 
-   * @element-type ElementCircuit
-   */
-  private Vector  elements;
-  
-  public Boolean update() {
-  return null;
-  }
+	@Getter
+	private ArrayList<ElementCircuit>  elements;
 
-  public ElementCircuit calculChemin(Noeud depart, Noeud arrivee) {
-  return null;
-  }
+	/**
+	 * Appelle la méthode update de tous les éléments du circuit.
+	 * @return Renvoie true si tous les éléments ont été correctement mis à jour.
+	 */
+	public Boolean update() {
+		Boolean allUpdated = true;
+		for (int i = 0; i < elements.size(); i++) {
+			if(!elements.get(i).update()) {
+				allUpdated = false;
+			}
+		}
+		return allUpdated;
+	}
+
+	/**
+	 * Renvoie un chemin entre le noeud depart et le noeud arrivee.
+	 * @param depart Le noeud duquel on part.
+	 * @param arrivee Le noeud auquel on souhaite arriver.
+	 * @return Renvoie le chemin 
+	 */
+	public ArrayList<ElementCircuit> calculChemin(Noeud depart, Noeud arrivee) {
+		return null;
+	}
 
 }
