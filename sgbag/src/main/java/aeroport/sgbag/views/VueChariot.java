91ebc4c1923533905f766737dbdc1ba1f2d9f722
Michael Fagno <michael.fagno@free.fr>
2011-12-05 11:51:29 +0100

    vueNoeud constructeur ajouté

--- sgbag/src/main/java/aeroport/sgbag/views/VueChariot.java
+++ sgbag/src/main/java/aeroport/sgbag/views/VueChariot.java
@@ -1,12 +1,26 @@
 package aeroport.sgbag.views;
 
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.widgets.Canvas;
+
+import aeroport.sgbag.kernel.Chariot;
+
 public class VueChariot extends VueElem {
 	
-	public void updateView(){
-			
+	public VueChariot(Canvas parent, Chariot chariot) {
+		super(parent);
+		this.image = new Image(parent.getDisplay(), "data/img/chariot.png");
+		this.chariot = chariot;
 	}
 	
-	public void draw(){
+	public void updateView(){
 		
+		}
+	
+	public void draw(){
+		chariot.getPosition(); 
+		//drawImage(getImage(), X, Y);
 	}
+	
+	private Chariot chariot;
 }
