e12ffa54e08945363a60e195c9c2ee2e796b1ce8
mica <michael.fagno@free.fr>
2011-12-05 09:22:34 +0100

    Implementation partielle de ConnexionCircuit

--- sgbag/src/main/java/aeroport/sgbag/kernel/ConnexionCircuit.java
+++ sgbag/src/main/java/aeroport/sgbag/kernel/ConnexionCircuit.java
@@ -1,8 +1,34 @@
 package aeroport.sgbag.kernel;
+
+import lombok.Getter;
+import lombok.Setter;
+
 /*
  */
 public class ConnexionCircuit extends Noeud {
 
+	@Getter
+	@Setter
     private FileBagage fileBagage;
+    
+    public Boolean update(){
+    	if( this.hasChariot() ){
+    		if( this.getListeChariot().getFirst().getDestination() == this){
+    			if(this.fileBagage instanceof TapisRoulant){
+    				//if((TapisRoulant)fileBagage.hasReadyBagage()){
+    					this.getListeChariot().getFirst().addBagage(null);//(TapisRoulant)fileBagage.getBagageIfReady()
+    					//moveToNextRail();
+    				//}   				
+    			}
+    			else{//Type toboggan
+    				this.getListeChariot().getFirst().moveBagageToFile(fileBagage);
+    				//moveToNextRail();
+    			}
+    		}else{
+    			//moveToNextRail();
+    		}  			
+    	}
+    	return null;
+    }
 
 }
