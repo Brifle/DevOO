78be7ee72f3e5658ece5c523eececc6a79f76a0c
mathieu <sabourin.mathieu@free.fr>
2011-12-05 23:14:09 +0100

    Fixed imports in Views

--- sgbag/src/main/java/aeroport/sgbag/views/VueRail.java
+++ sgbag/src/main/java/aeroport/sgbag/views/VueRail.java
@@ -3,13 +3,14 @@
  */
 package aeroport.sgbag.views;
 
-import lombok.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import org.eclipse.swt.graphics.GC;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.graphics.Transform;
-import org.eclipse.swt.widgets.Canvas;
 
 import aeroport.sgbag.kernel.Rail;
 
