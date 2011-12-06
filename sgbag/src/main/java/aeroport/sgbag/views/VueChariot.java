73806eb7ade42735a263bb4c5b451a835b01109c
mathieu <sabourin.mathieu@free.fr>
2011-12-06 17:47:58 +0100

    FFFFFFFFFFFFFFFFFFFFFFFFFFFUUUUUUUUUUUUUUUUUUUU-

--- sgbag/src/main/java/aeroport/sgbag/views/VueChariot.java
+++ sgbag/src/main/java/aeroport/sgbag/views/VueChariot.java
@@ -1,33 +1,17 @@
 package aeroport.sgbag.views;
 
 import lombok.Getter;
-<<<<<<< HEAD
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import org.eclipse.swt.graphics.GC;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.Rectangle;
-import org.eclipse.swt.graphics.Transform;
 import org.eclipse.swt.widgets.Canvas;
 
 import aeroport.sgbag.kernel.Chariot;
-import aeroport.sgbag.kernel.Rail;
 
 @NoArgsConstructor
-=======
-import lombok.Setter;
-
-import org.eclipse.swt.graphics.GC;
-import org.eclipse.swt.graphics.Image;
-import org.eclipse.swt.graphics.Rectangle;
-import org.eclipse.swt.graphics.Transform;
-import org.eclipse.swt.widgets.Canvas;
-
-import aeroport.sgbag.kernel.Chariot;
-import aeroport.sgbag.kernel.Rail;
-
->>>>>>> branch 'master' of git@github.com:Brifle/DevOO.git
 public class VueChariot extends VueElem {
 
 	@Getter
