5025ffc2735dbec7aa4a623610fa82dbd1f75844
tpatel <thibaut.patel@gmail.com>
2011-12-07 00:07:24 +0100

    Ajout des logs.

--- sgbag/src/main/java/aeroport/sgbag/kernel/ConnexionCircuit.java
+++ sgbag/src/main/java/aeroport/sgbag/kernel/ConnexionCircuit.java
@@ -2,9 +2,9 @@ package aeroport.sgbag.kernel;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.extern.log4j.Log4j;
 
-/*
- */
+@Log4j
 public class ConnexionCircuit extends Noeud {
 
 	@Getter
@@ -12,6 +12,7 @@ public class ConnexionCircuit extends Noeud {
 	private FileBagage fileBagage;
 
 	public Boolean update() {
+		log.trace("Update de la ConnexionCircuit " + this);
 		if(hasChariot()) {
 			if(getListeChariot().getFirst().getDestination() == this) { //Chariot arrivé à destination
 				if(fileBagage instanceof TapisRoulant) {
