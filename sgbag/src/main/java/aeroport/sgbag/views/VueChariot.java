052f666936040031bb9ac08f9b7ef1a120d296ac
Michael Fagno <michael.fagno@free.fr>
2011-12-06 22:52:22 +0100

    suppression de draw() + correction du constructeur

--- sgbag/src/main/java/aeroport/sgbag/views/VueChariot.java
+++ sgbag/src/main/java/aeroport/sgbag/views/VueChariot.java
@@ -19,44 +19,18 @@ public class VueChariot extends VueElem {
 	@Setter
 	private Chariot chariot;
 
-	@Getter
-	@Setter
-	private GC gc;
-
 	public VueChariot(Canvas parent, Chariot chariot) {
 		super((VueHall) parent);
 		this.image = new Image(parent.getDisplay(), "data/img/chariot.png");
-		gc = this.parent.getGcBuffer();
 		
-		height = chariot.getLength();
 		Rectangle rect = image.getBounds();
-		width = rect.height * chariot.getLength() / rect.width;
+		width  = chariot.getLength();
+		height = width * rect.height/ rect.width;
+		
 	}
 
-	public void updateView() {
-				
+	public void updateView() {			
 		//TODO
-
 	}
 
-	public void draw() {
-		GC gc = this.parent.getGcBuffer();
-
-		// We get the size of the original image :
-		Rectangle rect = image.getBounds();
-
-		// We create a transform in order to rotate and translate the image :
-		Transform trImage = new Transform(parent.getDisplay());
-		trImage.translate(this.x, this.y);
-		trImage.rotate(this.angle);
-		gc.setTransform(trImage);
-
-		// Then we just draw the image on the GC :
-		gc.drawImage(this.image, 0, 0, rect.width, rect.height, -this.width / 2,
-				-this.height / 2, this.width, this.height);
-
-		// We no longer need the transform :
-		gc.setTransform(null);
-		trImage.dispose();
-	}
 }
