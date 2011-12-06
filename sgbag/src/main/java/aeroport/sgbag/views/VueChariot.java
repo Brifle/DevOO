57653c646b1eb04c1dd580f5808951bb411cdb6f
mathieu <sabourin.mathieu@free.fr>
2011-12-06 17:33:50 +0100

    Finished task 3 (Implementation of Selector)

--- sgbag/src/main/java/aeroport/sgbag/views/VueChariot.java
+++ sgbag/src/main/java/aeroport/sgbag/views/VueChariot.java
@@ -1,12 +1,50 @@
 package aeroport.sgbag.views;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import org.eclipse.swt.graphics.GC;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.graphics.Rectangle;
+import org.eclipse.swt.graphics.Transform;
+import org.eclipse.swt.widgets.Canvas;
+
+import aeroport.sgbag.kernel.Chariot;
+import aeroport.sgbag.kernel.Rail;
+
+@NoArgsConstructor
 public class VueChariot extends VueElem {
-	
-	public void updateView(){
-			
+
+	@Getter
+	@Setter
+	private Chariot chariot;
+
+	@Getter
+	@Setter
+	private GC gc;
+
+	public VueChariot(Canvas parent, Chariot chariot) {
+		super((VueHall) parent);
+		this.image = new Image(parent.getDisplay(), "data/img/chariot.png");
 	}
-	
-	public void draw(){
-		
+
+	public void updateView() {
+
+	}
+
+	public void draw() {
+
+		Rectangle rect = image.getBounds();
+
+		// draw the chariot on the rail
+		gc.drawImage(this.image, 0, 0, rect.width, rect.height, 0,
+				chariot.getPosition(), chariot.getLength(),
+				rect.height * chariot.getLength() / rect.width);
+
+		if (!chariot.isEmpty()) {
+			// getView(chariot.getBagage()).draw() TODO
+		}
+
 	}
 }
