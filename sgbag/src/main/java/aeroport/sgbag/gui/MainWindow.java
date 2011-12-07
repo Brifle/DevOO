6aec0a08d44f756e36e4ba159a029f8470cffedf
Stanislas Signoud <signez@stanisoft.net>
2011-12-07 03:21:11 +0100

    Improving MainWindow, to test SWT features.
    (If someone understand why swt add some jars...)

--- sgbag/src/main/java/aeroport/sgbag/gui/MainWindow.java
+++ sgbag/src/main/java/aeroport/sgbag/gui/MainWindow.java
@@ -10,16 +10,30 @@ import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
-import org.eclipse.swt.widgets.Canvas;
+import org.eclipse.wb.swt.SWTResourceManager;
 
 import aeroport.sgbag.views.VueHall;
-import org.eclipse.wb.swt.SWTResourceManager;
-import org.eclipse.swt.custom.StackLayout;
+import org.eclipse.wb.swt.layout.grouplayout.GroupLayout;
+import org.eclipse.jface.fieldassist.ControlDecoration;
+import org.eclipse.jface.action.Action;
+import org.junit.runners.ParentRunner;
+import org.eclipse.swt.widgets.Tree;
+import org.eclipse.wb.swt.layout.grouplayout.LayoutStyle;
+import org.eclipse.swt.widgets.Scale;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.custom.CLabel;
 import org.eclipse.swt.layout.FormLayout;
 import org.eclipse.swt.layout.FormData;
 import org.eclipse.swt.layout.FormAttachment;
 
+/**
+ * SGBag GUI root window.
+ * 
+ * @author Stanislas Signoud <signez@stanisoft.net>
+ *
+ */
 public class MainWindow extends ApplicationWindow {
+	private Action actionQuitter;
 
 	/**
 	 * Create the application window.
@@ -38,12 +52,50 @@ public class MainWindow extends ApplicationWindow {
 	 */
 	@Override
 	protected Control createContents(Composite parent) {
+		parent.setSize(new Point(400, 400));
 		Composite container = new Composite(parent, SWT.NONE);
-		container.setLayout(null);
 		
-		VueHall vueHall = new VueHall(container, SWT.NONE);
-		vueHall.setBounds(0, 0, 640, 287);
+		VueHall vueHall = new VueHall(container, SWT.BORDER);
 		vueHall.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
+		
+		Tree tree = new Tree(container, SWT.BORDER);
+		
+		Scale scale = new Scale(container, SWT.NONE);
+		
+		CLabel lblHorloge_1 = new CLabel(container, SWT.NONE);
+		lblHorloge_1.setAlignment(SWT.RIGHT);
+		lblHorloge_1.setText("Horloge");
+		GroupLayout gl_container = new GroupLayout(container);
+		gl_container.setHorizontalGroup(
+			gl_container.createParallelGroup(GroupLayout.LEADING)
+				.add(gl_container.createSequentialGroup()
+					.add(12)
+					.add(gl_container.createParallelGroup(GroupLayout.LEADING)
+						.add(GroupLayout.TRAILING, gl_container.createSequentialGroup()
+							.addPreferredGap(LayoutStyle.RELATED)
+							.add(lblHorloge_1, GroupLayout.DEFAULT_SIZE, 151, Short.MAX_VALUE)
+							.addPreferredGap(LayoutStyle.RELATED)
+							.add(scale, GroupLayout.PREFERRED_SIZE, 106, GroupLayout.PREFERRED_SIZE))
+						.add(vueHall, GroupLayout.DEFAULT_SIZE, 263, Short.MAX_VALUE))
+					.addPreferredGap(LayoutStyle.RELATED)
+					.add(tree, GroupLayout.DEFAULT_SIZE, 179, Short.MAX_VALUE)
+					.add(0))
+		);
+		gl_container.setVerticalGroup(
+			gl_container.createParallelGroup(GroupLayout.LEADING)
+				.add(gl_container.createSequentialGroup()
+					.add(12)
+					.add(gl_container.createParallelGroup(GroupLayout.LEADING)
+						.add(tree, GroupLayout.DEFAULT_SIZE, 356, Short.MAX_VALUE)
+						.add(gl_container.createSequentialGroup()
+							.add(vueHall, GroupLayout.DEFAULT_SIZE, 317, Short.MAX_VALUE)
+							.addPreferredGap(LayoutStyle.RELATED)
+							.add(gl_container.createParallelGroup(GroupLayout.TRAILING)
+								.add(lblHorloge_1, GroupLayout.PREFERRED_SIZE, 33, GroupLayout.PREFERRED_SIZE)
+								.add(scale, GroupLayout.PREFERRED_SIZE, 33, GroupLayout.PREFERRED_SIZE))))
+					.add(0))
+		);
+		container.setLayout(gl_container);
 
 		return container;
 	}
@@ -53,6 +105,15 @@ public class MainWindow extends ApplicationWindow {
 	 */
 	private void createActions() {
 		// Create the actions
+		{
+			actionQuitter = new Action("Quitter") {
+				@Override
+				public void run() {
+					super.run();
+					close();
+				}
+			};
+		}
 	}
 
 	/**
@@ -61,8 +122,13 @@ public class MainWindow extends ApplicationWindow {
 	 */
 	@Override
 	protected MenuManager createMenuManager() {
-		MenuManager menuManager = new MenuManager("menu");
-		return menuManager;
+		MenuManager menuBar = new MenuManager("menu");
+		{
+			MenuManager menuFichier = new MenuManager("Fichier");
+			menuBar.add(menuFichier);
+			menuFichier.add(actionQuitter);
+		}
+		return menuBar;
 	}
 
 	/**
@@ -108,13 +174,5 @@ public class MainWindow extends ApplicationWindow {
 	protected void configureShell(Shell shell) {
 		super.configureShell(shell);
 		shell.setText("SGBag");
-	}
-
-	/**
-	 * Return the initial size of the window.
-	 */
-	@Override
-	protected Point getInitialSize() {
-		return new Point(513, 351);
 	}
 }
