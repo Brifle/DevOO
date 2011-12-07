58635885fc4fd1d021d73b37c200ebcacd0f4af0
Arnaud Lahache <arnaud.lahache@gmail.com>
2011-12-07 15:21:23 +0100

    added updateView in VueChariot

---
+++ sgbag/src/test/java/aeroport/sgbag/views/VueChariotTest.java
@@ -0,0 +1,40 @@
+package aeroport.sgbag.views;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import aeroport.sgbag.controler.ViewSelector;
+import aeroport.sgbag.kernel.Chariot;
+import aeroport.sgbag.kernel.Rail;
+
+public class VueChariotTest {
+
+	@Test
+	public void testUpdateView() {
+		
+		Rail rail = new Rail(100);
+		Chariot chariot = new Chariot(10, 5, 10, null, null, null);
+		rail.registerChariot(chariot);
+		chariot.setParent(rail);
+		
+		VueRail vueRail = new VueRail();
+		vueRail.setRail(rail);
+		vueRail.setAngle(0);
+		vueRail.setWidth(100);
+		vueRail.setX(50);
+		vueRail.setY(0);
+		ViewSelector.getInstance().setKernelView(rail, vueRail);
+		
+		VueChariot vueChariot = new VueChariot();
+		vueChariot.setChariot(chariot);
+		ViewSelector.getInstance().setKernelView(chariot, vueChariot);
+		
+		vueChariot.updateView();
+		
+		assertEquals(10, chariot.getPosition());
+		assertEquals(10, vueChariot.x);
+		assertEquals(0, vueChariot.y);
+	}
+
+}
