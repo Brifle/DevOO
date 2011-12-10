e0779d4bbfaac58dd6adb66afb76ba0104303630
Michael Fagno <michael.fagno@free.fr>
2011-12-10 12:28:22 +0100

    Ajout de getAngleSegment

--- sgbag/src/main/java/aeroport/sgbag/utils/Geom.java
+++ sgbag/src/main/java/aeroport/sgbag/utils/Geom.java
@@ -20,5 +20,37 @@ public class Geom {
 
 		return new Point(newX, newY);
 	}
+	
+	/**
+	 * Permet de connaitre l'angle du segment [p1,p2] par rapport à  l'axe x (cf canvas)
+	 * 
+	 * @param p1 Extrémité du rail
+	 * @param p2 Extrémité du rail
+	 * @return l'angle du segment [p1,p2] par rapport à  l'axe x (cf canvas)
+	 */
+		public double getAngleSegment(Point p1, Point p2) {
+	 
+			double angle;
+
+			// Point au milieu des 2 points
+			Point centre = new Point((p2.x + p1.x) / 2, (p2.y + p1.y) / 2);
+
+			double hypo = Math.sqrt(Math.pow(centre.x - p1.x, 2)
+					+ Math.pow(centre.y - p1.y, 2));
+			double adjacent = Math.sqrt(Math.pow(centre.x - p1.x, 2));
+
+			if (hypo != 0) {
+				angle = Math.acos(adjacent / hypo);
+
+				if ((p1.x < centre.x && p1.y > centre.y)
+						|| (p2.x < centre.x && p2.y > centre.y)) {
+					angle = -angle;
+				}
+				System.out.println(angle);
+			} else {
+				angle = Math.PI / 2;
+			}
+			return angle;
+		}
 
 }
