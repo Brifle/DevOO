d0d977000a4eb815f004c43e0e272461c47f1a6e
Arnaud Lahache <arnaud.lahache@gmail.com>
2011-12-10 17:39:18 +0100

    init() method in Clock

--- sgbag/src/main/java/aeroport/sgbag/controler/Clock.java
+++ sgbag/src/main/java/aeroport/sgbag/controler/Clock.java
@@ -9,16 +9,26 @@ public class Clock implements Runnable {
 	
 	@Getter
 	@Setter
+	private int interval;
+	
+	@Getter
+	@Setter
 	private Hall hall;
 	
 	@Getter
 	@Setter
 	private VueHall vueHall;
+	
+	public void init() {
+		vueHall.getDisplay().timerExec(interval, this);
+	}
 
 	public void run() {
 		hall.update();
 		vueHall.update();
 		vueHall.draw();
+		
+		vueHall.getDisplay().timerExec(interval, this);
 	}
 
 }
