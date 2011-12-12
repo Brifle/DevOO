64063068e759742efb9540eb795593f1e5502314
jonas <jonasbru@wanadoo.fr>
2011-12-12 22:33:38 +0100

    PropertiesWidget Rail Charriot Bagage

--- sgbag/src/main/java/aeroport/sgbag/gui/PropertiesWidget.java
+++ sgbag/src/main/java/aeroport/sgbag/gui/PropertiesWidget.java
@@ -21,6 +21,8 @@ import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.Spinner;
 
 import aeroport.sgbag.controler.Simulation;
+import aeroport.sgbag.kernel.Bagage;
+import aeroport.sgbag.kernel.Chariot;
 import aeroport.sgbag.kernel.KernelObject;
 import aeroport.sgbag.kernel.Rail;
 import aeroport.sgbag.kernel.TapisRoulant;
@@ -78,11 +80,11 @@ public class PropertiesWidget extends Composite {
 				l.setText("Aucun objet selectionné");
 				l.setLayoutData(new GridData(SWT.FILL, SWT.FILL, false, false,
 						2, 1));
-			} else {				
+			} else {
 				if (this.simulation.getSelectedElem() instanceof VueBagage) {
-
+					setVueBagageMode();
 				} else if (this.simulation.getSelectedElem() instanceof VueChariot) {
-
+					setVueChariotMode();
 				} else if (this.simulation.getSelectedElem() instanceof VueRail) {
 					setVueRailMode();
 				} else if (this.simulation.getSelectedElem() instanceof VueToboggan) {
@@ -97,27 +99,77 @@ public class PropertiesWidget extends Composite {
 		this.layout();
 	}
 	
-	private void setVueRailMode(){
-		Rail tr = ((VueRail) this.simulation.getSelectedElem())
-				.getRail();
+	private void setVueBagageMode() {
+		Bagage tr = ((VueBagage) this.simulation.getSelectedElem())
+				.getBagage();
+
+		setCommonProperties(tr);
+		
+		if (tr.getDestination() != null && tr.getDestination().getName() != null) {
+			new Label(canvas, SWT.NONE).setText("Noeud destination : ");
+			new Label(canvas, SWT.NONE).setText(tr.getDestination()
+					.getName());
+		}
+		
+		new Label(canvas, SWT.NONE).setText("Position relative : ");
+		new Label(canvas, SWT.NONE).setText("" + tr.getPosition());
+	}
+
+	private void setVueChariotMode() {
+		Chariot tr = ((VueChariot) this.simulation.getSelectedElem())
+				.getChariot();
+
+		setCommonProperties(tr);
+
+		new Label(canvas, SWT.NONE).setText("Mouvement maximal : ");
+		Spinner s1 = new Spinner(canvas, SWT.WRAP);
+		s1.addModifyListener(new ModifyListener() {
+			@Override
+			public void modifyText(ModifyEvent arg0) {
+				Chariot tr = ((VueChariot) simulation.getSelectedElem())
+						.getChariot();
+
+				tr.setMaxMoveDistance(((Spinner) arg0.widget).getSelection());
+			}
+		});
+		s1.setValues(tr.getMaxMoveDistance(), 0, 100, 0, 1, 10);
+		
+		new Label(canvas, SWT.NONE).setText("Position relative : ");
+		new Label(canvas, SWT.NONE).setText("" + tr.getPosition());
+
+		if (tr.getDestination() != null && tr.getDestination().getName() != null) {
+			new Label(canvas, SWT.NONE).setText("Noeud destination : ");
+			new Label(canvas, SWT.NONE).setText(tr.getDestination()
+					.getName());
+		}
+
+		if (tr.getBagage() != null && tr.getBagage().getName() != null) {
+			new Label(canvas, SWT.NONE).setText("Bagage : ");
+			new Label(canvas, SWT.NONE).setText(tr.getBagage().getName());
+		}
+	}
+
+	private void setVueRailMode() {
+		Rail tr = ((VueRail) this.simulation.getSelectedElem()).getRail();
 
 		setCommonProperties(tr);
 
 		new Label(canvas, SWT.NONE).setText("Longueur du rail : ");
-		new Label(canvas, SWT.NONE).setText(""+tr.getLength());
+		new Label(canvas, SWT.NONE).setText("" + tr.getLength());
 
-		if(tr.getNoeudPrecedent() != null){
+		if (tr.getNoeudPrecedent() != null) {
 			new Label(canvas, SWT.NONE).setText("Noeud precedent : ");
-			new Label(canvas, SWT.NONE).setText(tr.getNoeudPrecedent().getName());
+			new Label(canvas, SWT.NONE).setText(tr.getNoeudPrecedent()
+					.getName());
 		}
-		
-		if(tr.getNoeudSuivant() != null){
+
+		if (tr.getNoeudSuivant() != null) {
 			new Label(canvas, SWT.NONE).setText("Noeud suivant : ");
 			new Label(canvas, SWT.NONE).setText(tr.getNoeudSuivant().getName());
 		}
 	}
-	
-	private void setCommonProperties(KernelObject o){
+
+	private void setCommonProperties(KernelObject o) {
 		new Label(canvas, SWT.NONE).setText("Nom : " + o.getName());
 
 		new Label(canvas, SWT.NONE).setText("Id : " + o.getId());
@@ -126,7 +178,7 @@ public class PropertiesWidget extends Composite {
 	private void setVueToboganMode() {
 		Toboggan tr = ((VueToboggan) this.simulation.getSelectedElem())
 				.getToboggan();
-		
+
 		setCommonProperties(tr);
 
 		Label l1 = new Label(canvas, SWT.NONE);
@@ -167,7 +219,7 @@ public class PropertiesWidget extends Composite {
 				.getTapisRoulant();
 
 		setCommonProperties(tr);
-		
+
 		Label l1 = new Label(canvas, SWT.NONE);
 		l1.setText("Vitesse du tapis :");
 
