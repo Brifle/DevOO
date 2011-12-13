4ec67c3a78b98ed525561111bd9793793c199157
Michael Fagno <michael.fagno@free.fr>
2011-12-13 17:54:20 +0100

    VueTapisRoulant set pos

--- sgbag/src/main/java/aeroport/sgbag/views/VueTapisRoulant.java
+++ sgbag/src/main/java/aeroport/sgbag/views/VueTapisRoulant.java
@@ -1,12 +1,16 @@
 package aeroport.sgbag.views;
 
+import org.eclipse.swt.graphics.GC;
 import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.graphics.Rectangle;
+import org.eclipse.swt.graphics.Transform;
 import org.eclipse.swt.widgets.Canvas;
 
 import lombok.Getter;
 import lombok.Setter;
 import aeroport.sgbag.kernel.TapisRoulant;
+import aeroport.sgbag.utils.Geom;
 
 public class VueTapisRoulant extends VueElem {
 
@@ -24,6 +28,7 @@ public class VueTapisRoulant extends VueElem {
 		Rectangle rect = image.getBounds();
 		this.width = rect.width;
 		this.height = rect.height;
+		this.x = this.x - 30;
 	}
 
 	@Override
