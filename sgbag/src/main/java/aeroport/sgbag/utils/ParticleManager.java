db29ddd8cd49416f57b506969b8ef44563a70d65
Stanislas Signoud <signez@stanisoft.net>
2011-12-15 20:07:35 +0100

    Commenting ParticleManager.

--- sgbag/src/main/java/aeroport/sgbag/utils/ParticleManager.java
+++ sgbag/src/main/java/aeroport/sgbag/utils/ParticleManager.java
@@ -2,12 +2,21 @@ package aeroport.sgbag.utils;
 
 import org.eclipse.swt.graphics.GC;
 
+/**
+ * Gère les particules.
+ * 
+ * @author Thibaut Patel
+ */
 public class ParticleManager {
 	private Particle[] particles;
 	private int next;
 	
 	private static ParticleManager particleManager = null;
 	
+	/**
+	 * Retourne une instance du gestionnaire de particule (singleton).
+	 * @return Instance de ParticleManager.
+	 */
 	public static ParticleManager getParticleManager() {
 		if(particleManager == null) {
 			particleManager = new ParticleManager();
@@ -15,10 +24,17 @@ public class ParticleManager {
 		return particleManager;
 	}
 	
+	/**
+	 * Crée un gestionnaire de particule.
+	 */
 	private ParticleManager() {
 		this(150);
 	}
 	
+	/**
+	 * Crée un gestionnaire de particule, contenant number particules.
+	 * @param number Le nombre de particules à gérer.
+	 */
 	private ParticleManager(int number) {
 		particles = new Particle[number];
 		for (int i = 0; i < particles.length; i++) {
@@ -27,18 +43,30 @@ public class ParticleManager {
 		next = -1;
 	}
 	
+	/**
+	 * Mets à jour les coordonnées de toutes les particules gérées.
+	 */
 	public void update() {
 		for (int i = 0; i < particles.length; i++) {
 			particles[i].update();
 		}
 	}
 	
+	/**
+	 * Affiche toutes les particules gérées.
+	 * @param buffer
+	 */
 	public void draw(GC buffer) {
 		for (int i = 0; i < particles.length; i++) {
 			particles[i].draw(buffer);
 		}
 	}
 	
+	/**
+	 * Jette une particule d'une position donnée.
+	 * @param x Position horizontale de jet de la particule.
+	 * @param y Position verticale de jet de la particule.
+	 */
 	public void throwParticle(float x, float y) {
 		next = (++next) % particles.length;
 		int sensx = (Math.random() < 0.5 ? 1 : -1);
