34fd98553efd7f5c9299c75a6bc17b94cdcdfa4e
Stanislas Signoud <signez@stanisoft.net>
2011-12-15 15:07:09 +0100

    Commenting TapisRoulant.

--- sgbag/src/main/java/aeroport/sgbag/kernel/TapisRoulant.java
+++ sgbag/src/main/java/aeroport/sgbag/kernel/TapisRoulant.java
@@ -7,6 +7,11 @@ import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
 
+/**
+ * Modèle représentant un tapis roulant.
+ * 
+ * @author Jonàs Bru Monserrat, Thibaut Patel, Mathieu Sabourin
+ */
 @RequiredArgsConstructor
 @Log4j
 public class TapisRoulant extends FileBagage {
@@ -34,6 +39,10 @@ public class TapisRoulant extends FileBagage {
 
 	private int nbCharriotsEnChemin = 0;
 
+	/**
+	 * Indique si le tapis présente un bagage prêt à être déposé.
+	 * @return true si c'est le cas, false sinon.
+	 */
 	public boolean hasReadyBagage() {
 		for (Bagage b : this.listBagages) {
 			if (b.getPosition() == this.length) {
@@ -44,12 +53,21 @@ public class TapisRoulant extends FileBagage {
 		return false;
 	}
 
+	/**
+	 * Indique si le tapis possède assez de bagage pour accueillir
+	 * un nouveau bagage.
+	 * @return true si c'est le cas, false sinon.
+	 */
 	public boolean hasPlaceForBagage(){
 		return this.listBagages.isEmpty()
 				|| this.listBagages.get(this.listBagages.size() - 1)
 				.getPosition() >= distanceEntreBagages;
 	}
 
+	/**
+	 * Mets à jour le contenu du tapis roulant, en déplaçant les
+	 * bagages qu'il contient.
+	 */
 	public boolean update() {
 		log.trace("Update tapis roulant");
 		if (!hasReadyBagage()) {
@@ -83,6 +101,10 @@ public class TapisRoulant extends FileBagage {
 		return true;
 	}
 
+	/**
+	 * Retire un bagage de la file et le retourne, si c'est possible.
+	 * @return Un bagage si un bagage est disponible, null sinon.
+	 */
 	public Bagage getBagageIfReady() {
 		for (Bagage b : this.listBagages) {
 			if (b.getPosition() == this.length) {
