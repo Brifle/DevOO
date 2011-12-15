3c4421bd202bd40c2e4671fea0312b710f528252
Stanislas Signoud <signez@stanisoft.net>
2011-12-15 15:49:01 +0100

    Commenting Geom.

--- sgbag/src/main/java/aeroport/sgbag/utils/Geom.java
+++ sgbag/src/main/java/aeroport/sgbag/utils/Geom.java
@@ -2,8 +2,21 @@ package aeroport.sgbag.utils;
 
 import org.eclipse.swt.graphics.Point;
 
+/**
+ * Classe utilitaire de géométrie.
+ * 
+ * @author 
+ */
 public class Geom {
 
+	/**
+	 * Retourne la nouvelle position du point p, après une rotation par rapport 
+	 * à un point donné.
+	 * @param p Point à faire tourner.
+	 * @param centre Centre de la rotation.
+	 * @param angle Angle en degrés, de la rotation, en sens horaire.
+	 * @return Nouvelle position du point, après rotation.
+	 */
 	public static Point getRotatedPoint(Point p, Point centre, float angle) {
 		double angleRad = Math.toRadians(angle);
 
@@ -28,56 +41,49 @@ public class Geom {
 	 * @param p2 Extrémité du rail
 	 * @return l'angle du segment [p1,p2] par rapport à  l'axe x (cf canvas)
 	 */
-		public double getAngleSegment(Point p1, Point p2) {
+	public double getAngleSegment(Point p1, Point p2) {
 	 
-			double angle = 0;
+		double angle = 0;
 
-			// Point au milieu des 2 points
-			Point centre = new Point((p2.x + p1.x) / 2, (p2.y + p1.y) / 2);
+		// Point au milieu des 2 points
+		Point centre = new Point((p2.x + p1.x) / 2, (p2.y + p1.y) / 2);
 
-			double hypo = Math.sqrt(Math.pow(centre.x - p1.x, 2)
-					+ Math.pow(centre.y - p1.y, 2));
-			double adjacent = Math.sqrt(Math.pow(centre.x - p1.x, 2));
+		double hypo = Math.sqrt(Math.pow(centre.x - p1.x, 2)
+				+ Math.pow(centre.y - p1.y, 2));
+		double adjacent = Math.sqrt(Math.pow(centre.x - p1.x, 2));
 
-			if (hypo != 0) {
-				angle = Math.acos(adjacent / hypo);
+		if (hypo != 0) {
+			angle = Math.acos(adjacent / hypo);
 
-				//Si p1 en haut à droite
-				if (p1.x > centre.x && p1.y < centre.y){
-					angle = -angle + -Math.PI;
+			//Si p1 en haut à droite
+			if (p1.x > centre.x && p1.y < centre.y){
+				angle = -angle + -Math.PI;
+			}
+			
+			//Si p1 en bas à droite
+			if (p1.x > centre.x && p1.y > centre.y){
+				angle = angle + Math.PI;
+			}
+			
+			//Si p1 en bas à gauche
+			if (p1.x < centre.x && p1.y > centre.y){
+				angle = -angle;
+			}
+			
+			//Si horizontal de droite à gauche
+			if(p1.y == centre.y && p1.x > centre.x){
+					angle = Math.PI;
+			}
+			
+			if(p1.x == centre.x){
+				if(p1.y > centre.y){//Dirigé vers le bas
+					angle = -Math.PI / 2;
+				}else{
+					angle = + Math.PI / 2;
 				}
-				
-				//Si p1 en haut à gauche
-				if (p1.x < centre.x && p1.y < centre.y){
-					angle = angle;
-				}
-				
-				//Si p1 en bas à droite
-				if (p1.x > centre.x && p1.y > centre.y){
-					angle = angle + Math.PI;
-				}
-				
-				//Si p1 en bas à gauche
-				if (p1.x < centre.x && p1.y > centre.y){
-					angle = -angle;
-				}
-				
-				//Si horizontal de droite à gauche
-				if(p1.y == centre.y && p1.x > centre.x){
-						angle = Math.PI;
-				}
-				
-				if(p1.x == centre.x){
-					if(p1.y > centre.y){//Dirigé vers le bas
-						angle = -Math.PI / 2;
-					}else{
-						angle = + Math.PI / 2;
-					}
-				}
-				
-				//System.out.println(angle);
-			} 
-			return angle;
-		}
+			}
+		} 
+		return angle;
+	}
 
 }
