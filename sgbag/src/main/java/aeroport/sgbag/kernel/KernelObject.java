4eaaf841e82b583ca344f535e533da12cd032873
Stanislas Signoud <signez@stanisoft.net>
2011-12-15 14:51:18 +0100

    Commenting KernelObject.

--- sgbag/src/main/java/aeroport/sgbag/kernel/KernelObject.java
+++ sgbag/src/main/java/aeroport/sgbag/kernel/KernelObject.java
@@ -4,6 +4,14 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * Base de tous les objets métiers.
+ * 
+ * Ce modèle permet d'associer un id et un nom à tous les objets
+ * métiers, générés de manière cohérente.
+ * 
+ * @author Stanislas Signoud, Thibaut Patel
+ */
 @EqualsAndHashCode(exclude={"name"})
 public abstract class KernelObject {
 	private static int counter = 0;
@@ -17,6 +25,10 @@ public abstract class KernelObject {
 	@Setter
 	protected String name = null;
 	
+	/**
+	 * Retourne le nom de l'objet.
+	 * @return Chaîne contenant le nom de l'objet.
+	 */
 	public String getName(){
 		if(name == null){
 			name = kTypeName + " n°" + kTypeCounter;
