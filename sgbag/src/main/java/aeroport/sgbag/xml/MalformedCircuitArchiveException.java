2454da19b6051a70259983500eed7b4e50a25828
Stanislas Signoud <signez@stanisoft.net>
2011-12-16 02:12:45 +0100

    Commenting MalformedCircuitArchiveException.

--- sgbag/src/main/java/aeroport/sgbag/xml/MalformedCircuitArchiveException.java
+++ sgbag/src/main/java/aeroport/sgbag/xml/MalformedCircuitArchiveException.java
@@ -1,10 +1,14 @@
 package aeroport.sgbag.xml;
 
+/**
+ * Exception représentant l'impossibilité de comprendre le XML indiqué dans
+ * un fichier.
+ * 
+ * Cela signifie que l'arbre XML contenu dans le fichier ne correspond pas aux
+ * entités attendues, ou que l'arbre XML est mal formé.
+ * 
+ * @author Stanislas Signoud
+ */
 public class MalformedCircuitArchiveException extends Exception {
-
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
-
 }
